368c7d63ef35dca7313ecbbde9f0e706f36d9cc7
sourabh-yalagod <sourabhofficial99804@gmail.com>
2026-05-14 08:16:15 +0530

    test-4

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ config();
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ message: "App is Healthy and healthy....!" });
+  return res.json({ message: "App is Healthy and healthy with Full end to end CICD....!" });
 });
 
 const port = process.env.PORT || 3000;
