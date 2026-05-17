d4e7442d18320db1acae80888a7b99657f0353d8
sourabh-yalagod <sourabhofficial99804@gmail.com>
2026-05-17 09:09:23 +0530

    Chanages-2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ config();
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ message: "End to End CICD implementation." });
+  return res.json({ message: "Auto changes are implemented....!" });
 });
 
 const port = process.env.PORT || 3000;
